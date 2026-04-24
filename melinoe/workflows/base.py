2fc3616166da6460df835f488f77cd6b1b68616a
Nilton Teixeira <9078708+niltonfrederico@users.noreply.github.com>
2026-04-24 16:19:39 -0300

    refactor(melinoe): remove unused imports and __all__ definitions from __init__.py feat(workflows): add base classes for Step and Workflow to define reusable logic build(pyproject.toml): update Python version requirement to >=3.13,<3.14 build(pyproject.toml): add development dependencies for linting and debugging

---
+++ melinoe/workflows/base.py
@@ -0,0 +1,42 @@
+from typing import override
+from melinoe.client import ModelConfig
+from abc import ABC, abstractmethod
+
+
+class Step(ABC):
+    model_config: ModelConfig
+
+    def __init__(self):
+        self.validate_init()
+
+    def validate_init(self) -> None:
+        """
+        Validate that the Step has a ModelConfig attribute. This is required for any Step to function properly.
+        """
+        if not hasattr(self, "model_config") or not self.model_config:
+            raise ValueError("ModelConfig is required for Step initialization.")
+
+    def validate(self, *args, **kwargs) -> None:
+        """
+        Validate the input arguments for the step. This method should be overridden by subclasses to implement specific validation logic.
+        """
+        pass
+
+    @abstractmethod
+    def execute(self, *args, **kwargs):
+        """
+        Execute the step's main logic. This method must be implemented by subclasses to define the specific behavior of the step.
+        """
+        pass
+
+    def run(self, *args, **kwargs):
+        self.validate(*args, **kwargs)
+        return self.execute(*args, **kwargs)
+
+
+class Workflow(ABC):
+    steps: list[Step]
+
+    @abstractmethod
+    def run(self, *args, **kwargs):
+        pass
